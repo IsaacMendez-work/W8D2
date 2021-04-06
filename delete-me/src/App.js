b94ca7a8816a659d159967d36403ff5e640a11a3
Isaac Mendez <IsaacMendez.work@gmail.com>
2021-04-06 13:57:46 -0400

    completed project

--- delete-me/src/App.js
+++ delete-me/src/App.js
@@ -10,13 +10,12 @@ import Channel from './components/Channel';
 import Loader from './components/Loader';
 
 firebase.initializeApp({
-  apiKey: "AIzaSyCZr7sEJ-XgQnTJ2FWrKErtZA--uoEyegE",
-  authDomain: "lecture-firebase-f15af.firebaseapp.com",
-  databaseURL: "https://lecture-firebase-f15af-default-rtdb.firebaseio.com",
-  projectId: "lecture-firebase-f15af",
-  storageBucket: "lecture-firebase-f15af.appspot.com",
-  messagingSenderId: "206800055592",
-  appId: "1:206800055592:web:e90a8d3afb99b110cdf41d",
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_AUTH_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
 });
 
 const MoonIcon = props => (
